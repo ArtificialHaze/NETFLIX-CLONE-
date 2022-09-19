4b3f66b3b9009313bcf7807716851fe2a90aef52
ArtificialHaze <83704626+ArtificialHaze@users.noreply.github.com>
2022-09-19 14:53:20 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+// VARIABLES
+
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+// FUNCTIONS
+
+function selectItem() {
+  removeBorder();
+  removeShow();
+  this.classList.add("tab-border");
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  tabContentItem.classList.add("show");
+}
+
+function removeBorder() {
+  tabItems.forEach((item) => {
+    item.classList.remove("tab-border");
+  });
+}
+
+function removeShow() {
+  tabContentItems.forEach((item) => {
+    item.classList.remove("show");
+  });
+}
+
+// EVENT LISTENER
+
+tabItems.forEach((item) => {
+  item.addEventListener("click", selectItem);
+});
